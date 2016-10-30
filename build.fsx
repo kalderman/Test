f3db191ce7d94c0f5135c83939ad5a09e1a68835
kalderman <too.many.namespaces@gmail.com>
2016-10-29 22:12:56 -0400

    create and deploy nuget package

--- build.fsx
+++ build.fsx
@@ -2,20 +2,35 @@
 open Fake
 open Fake.Testing.XUnit2
 
-let projectName = "Lib"
+type ProjectInfo =
+  {
+    Name : string
+    Description : string
+    Authors : string list
+  }
+
+let projectInfo = 
+  {
+    Name = "Lib"
+    Description = "test"
+    Authors = [ "kalderman" ]
+  }
+let xunitConsolePath = ".tools/xunit.runner.console/tools/xunit.console.exe"
+
 let buildDir = "./build/"
 let buildProjectFiles = 
-  !! ("**/" + projectName + ".csproj")
+  !! ("**/" + projectInfo.Name + ".csproj")
 let testDir = "./test/"
 let testProjectFiles =
-  !! ("**/" + projectName + ".Tests.fsproj")
+  !! ("**/" + projectInfo.Name + ".Tests.fsproj")
 let testDlls =
   !! (testDir + "**/*.Tests.dll")
 let packagesDir = "./packages/"
 let isAppVeyorBuild = buildServer = BuildServer.AppVeyor
+let artifactsDir = "./artifacts/"
 
 let clean _ =
-    CleanDirs [buildDir; testDir; packagesDir;]
+    CleanDirs [buildDir; testDir; packagesDir; artifactsDir;]
 
 let buildRelease _ =
   MSBuildRelease buildDir "Build" buildProjectFiles
@@ -29,23 +44,59 @@ let testRelease _ =
   testDlls
     |> xUnit2 (fun p -> 
       { p with 
-          ToolPath = ".tools/xunit.runner.console/tools/xunit.console.exe"
+          ToolPath = xunitConsolePath
           ShadowCopy = false 
           ForceAppVeyor = isAppVeyorBuild})
 
 let restorePackages _ =
   RestorePackages()
 
+let buildNugetPackage _ = 
+  NuGet (fun p -> 
+      {p with
+        Authors = projectInfo.Authors
+        Project = projectInfo.Name
+        Description = projectInfo.Description
+        OutputPath = artifactsDir
+        Summary = ""
+        WorkingDir = buildDir
+        Version = (if buildVersion = "LocalBuild" then "0.0.0.0-pre" else buildVersion)
+        Publish = false 
+        Files = !! (buildDir + "*.dll") |> Seq.map (fun f -> (f, Some "lib", None)) |> Seq.toList }) 
+        (projectInfo.Name + ".nuspec")
+
+module AppVeyor =  
+  let execOnAppveyor arguments =
+    let result =
+      ExecProcess (fun info ->
+        info.FileName <- "appveyor"
+        info.Arguments <- arguments
+        ) (System.TimeSpan.FromMinutes 2.0)
+    if result <> 0 then failwith (sprintf "Failed to execute appveyor command: %s" arguments)
+
+  let publishNugetPackage folder =
+    !! (folder + "*.nupkg")
+    |> Seq.iter (fun artifact -> execOnAppveyor (sprintf "PushArtifact %s" artifact))
+
+let deployNugetPackage _ = 
+  AppVeyor.publishNugetPackage artifactsDir
+
 Target "Clean" clean
 Target "RestorePackages" restorePackages
 Target "BuildRelease" buildRelease 
 Target "BuildReleaseTests" buildReleaseTests
 Target "TestRelease" testRelease
+Target "BuildNugetPackage" buildNugetPackage
+Target "DeployNugetPackage" deployNugetPackage
+Target "Default" ignore
 
-"Clean" ==> "RestorePackages"
-"RestorePackages" ==> "BuildRelease"
-"RestorePackages" ==> "BuildReleaseTests"
-"BuildRelease" ==> "BuildReleaseTests"
-"BuildReleaseTests" ==> "TestRelease"
+"Clean" 
+  ==> "RestorePackages"
+  ==> "BuildRelease"
+  ==> "BuildReleaseTests"
+  ==> "TestRelease"
+  ==> "BuildNugetPackage"
+  =?> ("DeployNugetPackage", isAppVeyorBuild)
+  ==> "Default"
 
-RunTargetOrDefault "TestRelease"
+RunTargetOrDefault "Default"
